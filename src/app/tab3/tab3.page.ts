11b8232e24304bde6bffb045b7dd0440deb7e784
ednwoji <davidnwoji@gmail.com>
2023-12-08 01:55:03 +0100

    Modified profile page

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  firstName: any;
+  role: any;
+  userId: any;
+  darkMode = false;
 
-  constructor() {}
 
+  constructor(
+    private userService : UserService,
+
+  ) {
+    const userDetails = this.userService.getUserDetails();
+    this.firstName = userDetails?.firstName || 'Guest';
+    this.role = userDetails?.role;
+    this.userId = userDetails?.userId;
+    console.log('First Name is '+this.firstName+ ' and role is '+this.role);
+
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+  }
+
+  toggleRole() {
+    this.role = 'HOST';
+    console.log('Before toggle role is '+this.role);
+
+    this.role = this.role === 'HOST' ? 'TENANT' : this.role === 'TENANT' ? 'OTHER_ROLE' : 'HOST';
+    console.log('New role is '+this.role);
+  }
 }
